d6798277637bc02e2585c36d97fadb0ecdcf6fe4
sepehr <sepilami@ce.sharif.edu>
2019-09-04 00:58:50 +0430

    why are the hashmap's values some integer numbers?!!

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        File folder = new File("/home/sepehr/Downloads/");
+        File folder = new File("/home/sepehr/Downloads/test_files");
         ArrayList<String> realFiles = new ArrayList<>();
         ArrayList<String> paths = new ArrayList<>();
         System.out.println(System.currentTimeMillis());
+        TextFinder textFinder = new TextFinder();
         new RecursiveFileImporter(folder, realFiles, paths);
-        System.out.println(System.currentTimeMillis());
-        for (String realFile : paths) {
-            System.out.println(realFile);
+        for (String path : paths) {
+            File file = new File(path);
+            System.out.println("file is: " + path);
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String fileData = bufferedReader.readLine();
+                for (String string : fileData.split(" ")) {
+                    System.out.println(string);
+                    textFinder.data.putIfAbsent(string, new ArrayList());
+                    textFinder.data.get(string).add(file.getName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println(System.currentTimeMillis());
+        /*for (String realFile : paths) {
+            System.out.println(realFile);
+        }*/
+        System.out.println();
+        System.out.println(textFinder.data.values());
         System.out.println(realFiles.size());
     }
+
 }
---
+++ src/TextFinder.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TextFinder {
+
+    public HashMap<String, ArrayList<String>> data = new HashMap<>();
+
+
+}
