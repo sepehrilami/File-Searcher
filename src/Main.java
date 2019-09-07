feffe6d27feaa52c5524dbff72079f3471b9b05e
sepehr <sepilami@ce.sharif.edu>
2019-09-07 22:38:01 +0430

    a bit cleaner

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,9 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-
 public class Main {
 
-    public static void preprocess(Importer importer, Tokenizer tokenizer) {
-        File folder = new File("/home/sepehr/Downloads/test_files");
-        ArrayList<String> realFiles;
-        ArrayList<String> paths = new ArrayList<>();
-        System.out.println(System.currentTimeMillis());
-        TextFinder textFinder = new TextFinder();
-        RecursiveFileImporter recursiveFileImporter = new RecursiveFileImporter();
-        realFiles = recursiveFileImporter.readAllFiles(folder , paths);
-        for (String path : paths) {
-            File file = new File(path);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                String fileData = bufferedReader.readLine();
-                for (String string : fileData.split(" ")) {
-                    //System.out.println(string);
-                    textFinder.getData().putIfAbsent(string, new HashSet<>());
-                    textFinder.getData().get(string).add(file.getName());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(System.currentTimeMillis());
-        System.out.println(textFinder.getData().values().size());
-        System.out.println(realFiles.size());
-    }
-
     public static void main(String[] args) {
+        TextFinder textFinder = new TextFinder();
         Importer importer = null;
         Tokenizer tokenizer = null;
-        preprocess(importer , tokenizer);
+        textFinder.preprocess(importer , tokenizer);
     }
-
 }
--- src/TextFinder.java
+++ src/TextFinder.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -9,4 +14,29 @@ class TextFinder {
         return data;
     }
 
+    void preprocess(Importer importer, Tokenizer tokenizer) {
+        File folder = new File("/home/sepehr/Downloads/test_files");
+        ArrayList<String> realFiles;
+        ArrayList<String> paths = new ArrayList<>();
+        System.out.println(System.currentTimeMillis());
+        RecursiveFileImporter recursiveFileImporter = new RecursiveFileImporter();
+        realFiles = recursiveFileImporter.readAllFiles(folder , paths);
+        for (String path : paths) {
+            File file = new File(path);
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String fileData = bufferedReader.readLine();
+                for (String string : fileData.split(" ")) {
+                    //System.out.println(string);
+                    this.getData().putIfAbsent(string, new HashSet<>());
+                    this.getData().get(string).add(file.getName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis());
+        System.out.println(this.getData().values().size());
+        System.out.println(realFiles.size());
+    }
 }
