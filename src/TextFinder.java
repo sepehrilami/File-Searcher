f5ed189dd5937c0afd3b724f2ecee4529456a45c
sepehr <sepilami@ce.sharif.edu>
2019-09-08 09:05:25 +0430

    70000 words added to the list..and for each of them, list of files works properly

--- src/TextFinder.java
+++ src/TextFinder.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -20,25 +23,27 @@ class TextFinder {
         ArrayList<String> paths = new ArrayList<>();
         System.out.println(System.currentTimeMillis());
         RecursiveFileImporter recursiveFileImporter = new RecursiveFileImporter();
-        realFiles = recursiveFileImporter.readAllFiles(folder , paths);
-        for (String path : paths) {
-            File file = new File(path);
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                String fileData;
-                while((fileData = bufferedReader.readLine()) != null) {
-                    for (String string : fileData.split(" ")) {
-                        //System.out.println(string);
-                        this.getData().putIfAbsent(string, new HashSet<>());
-                        this.getData().get(string).add(file.getName());
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        realFiles = recursiveFileImporter.readAllFiles(folder, paths);
+        tokenCreate(paths);
         System.out.println(System.currentTimeMillis());
         System.out.println(this.getData().values().size());
         System.out.println(realFiles.size());
+    }
+
+    private void tokenCreate(ArrayList<String> paths) {
+        for (String path : paths) {
+            File file = new File(path);
+            try (BufferedReader ignored = new BufferedReader(new FileReader(file))) {
+
+                String fileData = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+                for (String string : fileData.split(" ")) {
+                    //System.out.println(string);
+                    this.getData().putIfAbsent(string, new HashSet<>());
+                    this.getData().get(string).add(file.getName());
+                }
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
