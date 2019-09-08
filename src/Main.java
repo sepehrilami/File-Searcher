5d06514fb4f40b925036d2131d377c0bb5b04c11
sepehr <sepilami@ce.sharif.edu>
2019-09-08 15:06:44 +0430

    a tiny upgrade :)

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         TextFinder textFinder = new TextFinder();
         Importer importer = null;
         Tokenizer tokenizer = null;
-        textFinder.preprocess(importer , tokenizer);
-        System.out.println("enter your word:");
+        textFinder.preprocess(importer, tokenizer);
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        System.out.println("Files containing this word:");
-        System.out.println(textFinder.getData().get(input));
+        while (true) {
+            System.out.println("enter your word:");
+            String input = scanner.next();
+            System.out.println("Files containing this word:\n");
+            if (textFinder.getData().get(input) != null) {
+                System.out.println(textFinder.getData().get(input));
+            } else {
+                System.out.println("Oops! We didn't find any file.\nTry something else!");
+            }
+        }
     }
 }
